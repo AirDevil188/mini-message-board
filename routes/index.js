22d6eacd077cd5cceddd17248fce800d6ff9e09f
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2024-05-14 11:16:45 +0200

    Added new route /new and in it an input form for new messages. Also, created form.pug template file for my form. In index.pug file added loop to render my messages. Change all var variables to const

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there, I hope that you have a nice day!",
+    user: "Amado025",
+    added: new Date(),
+  },
+  {
+    text: "Today I'm a new person.",
+    user: "NewPerson11",
+    added: new Date(),
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messaging Board", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New Message" });
 });
 
 module.exports = router;
